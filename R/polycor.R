3dcd0823383539b6071bffffb6c18c68ee7aecb4
mwelz <welz@ese.eur.nl>
2024-01-11 10:34:33 +0100

    finished integration of polcor C++ functions

---
+++ R/polycor.R
@@ -0,0 +1,292 @@
+# TODO: allow for data table argument like in polycor package
+# TODO: use objective_cpp() in polycor function and merge tertracor() with polycor()
+# TODO: maybe profile to see where the bottlenecks are (prolly the integral)
+# Rcpp::as<T>();
+# This C++ package has an implementation of linearly constrained optimization. Ask AA if we're allowed to use header files of it (and if we even need it to begin with)
+# TODO: implement feps() function like in the old R code; requires that 'mean' can be passed to model probs function DONE
+# TODO: Ask AA if we can drop monotonicity constraint (like fox); I don't think we should
+# TODO: make polycor() handle classes that are empirically zero
+# perhaps implement MLE variance explicitly? (fisher-info-based?)
+# TODO: chi square test for bivariate normality (see here: https://github.com/cran/polycor/blob/abb5d7b2ecc08ff282f2ee9dfeb89bce051229d9/R/polychor.R#L116). Implemented it, but I'm not sure how sensible this test is because it relies on cell empirical counts which are contaminated if there are outliers (so problem is in empirics, not the robustly estimated probabilities)
+# TODO: ask AA how to incorporate inference when there is contamination: start with rho=0? correct sign?
+# table(x, y) / n == matrix(fhat(x,y),5,5,byrow = T): clever trick to recover cell names!
+
+## chi square test for bivariate normality
+chisq_test <- function(x, y, theta, Kx, Ky)
+{
+  
+  ## prepare input
+  idxX <- 2:Kx
+  idxY <- (Kx+1):(Kx+Ky-1)
+  rho <- theta[1]
+  thresX <- c(-Inf, theta[idxX], Inf) 
+  thresY <- c(-Inf, theta[idxY], Inf)
+  
+  ## empirical and theoretical probabilities
+  f <- fhat(x = x, y = y, Kx = Kx, Ky = Ky)
+  p <- model_probabilities(rho = rho, thresX = thresX, 
+                           thresY = thresY, Kx = Kx, Ky = Ky)
+  N <- length(x)
+  counts <- f * N # empirical counts per class
+  
+  ## calculate the summands
+  summands <- counts * log(f / p)
+  teststat <- 2.0 * sum(summands[f > 0])
+  
+  ## degrees of freedom
+  #Kxx <- length(unique(x))
+  #Kyy <- length(unique(y))
+  #K <- sum(f > 0) # skip empty classes
+  #df <- K - Kxx - Kyy
+  df <- Kx * Ky - Kx - Ky
+  
+  ## p value
+  pval <- stats::pchisq(q = teststat, df = df, lower.tail = FALSE)
+  
+  return(list(chisq = teststat, df = df, pval = pval))
+}
+
+
+# return asymptotic covariance matrix
+polycor_variance <- function(theta, c1, c2, Kx, Ky, f, N)
+{
+  
+  ## prepare input
+  idxX <- 2:Kx
+  idxY <- (Kx+1):(Kx+Ky-1)
+  rho <- theta[1]
+  thresX <- c(-Inf, theta[idxX], Inf) 
+  thresY <- c(-Inf, theta[idxY], Inf) 
+  
+  # calculate M, W matrices
+  tmp <- get_MW(rho = rho, 
+                thresX = thresX, thresY = thresY,
+                f = f, c1 = c1, c2 = c2, Kx = Kx, Ky = Ky)
+  W <- tmp$W
+  M <- tmp$M
+  
+  ## calculate Lambda matrix
+  Lambda <- diag(f) - outer(f,f)
+  
+  ## calculate final matrix
+  U <- W %*% Lambda %*% t(W)
+  Minv <- solve(M)
+  Sigma <- Minv %*% U %*% Minv
+  colnames(Sigma) <- rownames(Sigma) <- theta_names(Kx = Kx, Ky = Ky)
+  
+  ## Sigma is the asy. covariance matrix of \sqrt(N)*thetahat
+  # thus, the asymptotic covariance matrix of thetahat is given by Sigma / N
+  return(list(variance = Sigma / N, Sigma = Sigma))
+}
+
+
+# MLE variance (based on inverted fisher information )
+polycor_variance_mle <- function(theta, Kx, Ky, N)
+{
+  ## prepare input
+  idxX <- 2:Kx
+  idxY <- (Kx+1):(Kx+Ky-1)
+  rho <- theta[1]
+  thresX <- c(-Inf, theta[idxX], Inf) 
+  thresY <- c(-Inf, theta[idxY], Inf) 
+  
+  
+  ## calculate fisher information matrix
+  fisher <- 
+    get_fisher(rho = rho, thresX = thresX, thresY = thresY, Kx = Kx, Ky = Ky)
+  
+  ## calculate asymptotic covariance matrix
+  Sigma <- solve(fisher)
+  
+  ## dimension naming
+  colnames(Sigma) <- rownames(Sigma) <- theta_names(Kx = Kx, Ky = Ky)
+  
+  ## Sigma is the asy. covariance matrix of \sqrt(N)*thetahat
+  # thus, the asymptotic covariance matrix of thetahat is given by Sigma / N
+  return(list(variance = Sigma / N, Sigma = Sigma))
+}
+
+
+# main functionality for polychoric correlation
+polycor_fast <- 
+  function(x = NULL, 
+           y = NULL,
+           Kx, Ky,
+           c1, c2,
+           method = "Nelder-Mead",
+           tol = 0.001,
+           init = c(0, init_thresholds(Kx), init_thresholds(Ky)), 
+           f = NULL,
+           variance = FALSE, # compute variance?
+           N = length(x),
+           chisq = FALSE) 
+{
+  K <- Kx * Ky
+  logc1p1 <- log(c1) + 1.0
+  logc2p1 <- log(c2) + 1.0
+  
+  # relative frequencies
+  if(is.null(f))
+  {
+    f. <-  fhat(x = x, y = y, Kx = Kx, Ky = Ky)
+  } else{
+    f. <- f
+  }
+  
+  ## local functions for optim
+  # theta is a vector of dimension (1 + (Kx-1) + (Ky-1)) = Kx+Ky-1
+  # theta[1] is rho
+  # theta[(1+1):(Kx-1+1)] = theta[2:Kx] are X-thresholds (increasing)
+  # theta[(Kx+1):(Kx+Ky-1)] are Y-thresholds (increasing)
+  idxX <- 2:Kx
+  idxY <- (Kx+1):(Kx+Ky-1)
+  feasible <- 1.0 - tol
+  fn <- function(theta)
+  {
+    rho <- theta[1]
+    thresX <- c(-Inf, theta[idxX], Inf) # first and last thresholds are fixed to be \mp Inf
+    thresY <- c(-Inf, theta[idxY], Inf) # first and last thresholds are fixed to be \mp Inf
+    objective_cpp_fast(rho = rho, f = f.,
+                       thresX = thresX, thresY = thresY, 
+                       c1 = c1, c2 = c2,
+                       Kx = Kx, Ky = Ky, K = K, 
+                       logc1p1 = logc1p1, logc2p1 = logc2p1,
+                       mean = c(0.0, 0.0),
+                       feasible = feasible)
+  }
+  
+  ## the linear constraints
+  lincon <- constrOptim_constraints(Kx = Kx, Ky = Ky, tol_rho = tol, tol_thresholds = tol)
+
+  ## linearly constrained optimization
+  opt <-
+    stats::constrOptim(theta = init,
+                       f = fn,
+                       grad = NULL,
+                       ui = lincon$ui,
+                       ci = lincon$ci,
+                       method = method,
+                       hessian = FALSE)
+  
+  ## without monotonicity constraints: runs faster, but less accurate
+  # opt <- optim(par = init, fn = fn, gr = NULL, 
+  #              lower = c(-1+tol, rep(-Inf, length(Kx+Ky-2))),
+  #              upper = c(1-tol, rep(Inf, length(Kx+Ky-2))), 
+  #              hessian = FALSE, method = "L-BFGS-B")
+  
+  ## extract and name estimated parameters
+  thetahat <- opt$par
+  names(thetahat) <- theta_names(Kx = Kx, Ky = Ky)
+  
+  ## if requested, estimate covariance matrix
+  if(variance)
+  {
+    tmp <- polycor_variance(theta = thetahat, c1 = c1, c2 = c2,
+                            Kx = Kx, Ky = Ky, f = f., N = N)
+    stderr <- sqrt(diag(tmp$variance))
+    Sigma  <- tmp$Sigma
+    
+  } else
+  {
+    Sigma <- stderr <- NULL
+  }
+  
+  ## perform chisq test for bivariate normality
+  if(chisq)
+  {
+    chisqtest <- chisq_test(x = x, y = y, 
+                            theta = thetahat, Kx = Kx, Ky = Ky)
+    chisq <- chisqtest$chisq
+    df <- chisqtest$df
+    pval <- chisqtest$pval
+  } else
+  {
+    chisq <- df <- pval <- NULL
+  }
+  
+  return(list(thetahat = thetahat, 
+              stderr = stderr,
+              sigma = Sigma,
+              chisq = chisq, 
+              df = df,
+              pval = pval,
+              objective = opt$value,
+              optim = opt))
+} # FUN
+
+
+
+#' Robust estimation of polychoric correlation coefficient
+#' 
+#' @param x vector of integer-valued responses to first item
+#' @param y vector of integer-valued responses to second item
+#' @param c tuning constant that governs robustness
+#' @param Kx number of response options in first item (defaults to \code{max(x)})
+#' @param Ky number of response options in second item (defaults to \code{max(y)})
+#' @param variance shall an estimated asymptotic covariance matrix be returned? Default is \code{TRUE}
+#' @param method numerical optimization method; default is Nelder-Mead
+#' @param tol tolerance in numerical optimization
+#' @param init initialization of numerical optimization. Default is neutral
+#' @param chisq shall a test for bivariate normality of teh latent variables be performed? Default is \code{FALSE}
+#' @export
+polycor <- function(x, y, c, 
+                    Kx = max(x),
+                    Ky = max(y),
+                    variance = TRUE,
+                    method = "Nelder-Mead",
+                    tol = 0.001,
+                    init = c(0, init_thresholds(Kx), init_thresholds(Ky)),
+                    chisq = FALSE)
+{
+  N <- length(x)
+  stopifnot(N == length(y))
+  stopifnot(c >= 1)
+  
+  polycor_fast(x = x, y = y, Kx = Kx, Ky = Ky, c1 = 0.0, c2 = c, 
+               method = method, tol = tol, init = init, 
+               f = NULL,
+               variance = variance, N = N, chisq = chisq)
+}
+
+
+#' Maximum likelihood estimation of polychoric correlation coefficient
+#' 
+#' @param x vector of integer-valued responses to first item
+#' @param y vector of integer-valued responses to second item
+#' @param Kx number of response options in first item (defaults to \code{max(x)})
+#' @param Ky number of response options in second item (defaults to \code{max(y)})
+#' @param variance shall an estimated asymptotic covariance matrix be returned? Default is \code{TRUE}
+#' @param method numerical optimization method; default is Nelder-Mead
+#' @param tol tolerance in numerical optimization
+#' @param init initialization of numerical optimization. Default is neutral
+#' @param chisq shall a test for bivariate normality of teh latent variables be performed? Default is \code{FALSE}
+#' @export
+polycor_mle <- function(x, y, 
+                        Kx = max(x),
+                        Ky = max(y),
+                        variance = TRUE,
+                        method = "Nelder-Mead",
+                        tol = 0.001,
+                        init = c(0, init_thresholds(Kx), init_thresholds(Ky)),
+                        chisq = FALSE)
+{
+  N <- length(x)
+  stopifnot(N == length(y))
+  
+  obj <- 
+    polycor_fast(x = x, y = y, Kx = Kx, Ky = Ky, c1 = 0.0, c2 = Inf, 
+                 method = method, tol = tol, init = init, 
+                 f = NULL,
+                 variance = FALSE, N = N, chisq = chisq)
+  
+  # if variance requested, calculate it based on fisher information
+  if(variance)
+  {
+    tmp <- 
+      polycor_variance_mle(theta = obj$thetahat, Kx = Kx, Ky = Ky, N = N)
+    obj$stderr <- sqrt(diag(tmp$variance))
+    obj$sigma <- tmp$Sigma
+  } 
+  
+  return(obj)
+}
---
+++ R/polycor_funs_helper.R
@@ -0,0 +1,91 @@
+# get thresholds for a given number of Likert points (=K)
+get_thresholds <- function(K)
+{
+  c(-Inf, 1:(K-1) - stats::median(1:(K-1)), Inf)
+} # FUN
+
+
+#' Initialize thresholds for K answer categories
+#' @param K number of answer categories
+#' @export
+init_thresholds <- function(K)
+{
+  tmp <- get_thresholds(K)
+  out <- tmp[-c(1, K+1)] # eliminiate the infinite fixed values
+  out
+} # FUN
+
+
+# get discretized versions of a latent variable, governed by the thresholds
+# TODO: Consider deleting this because we only need it in simulations
+get_discretization <- function(thresholds, latent)
+{
+  num_likert <- length(thresholds) - 1
+  y <- rep(NA_integer_, length(latent))
+  
+  for(j in 2:length(thresholds))
+  {
+    response <- j - 1L
+    y[thresholds[j-1] <= latent & latent < thresholds[j]] <- response
+  }
+  y
+}
+
+
+# get the matrix and vector that enforce the linear constraints in stats::constrOptim()
+constrOptim_constraints <- function(Kx, Ky, 
+                                    tol_rho = 0.001,
+                                    tol_thresholds = 0.001)
+{
+  Km1 <- Kx + Ky - 1L
+  Km2 <- Km1 - 1L
+  
+  ### the U matrix (ui in ?stats::constrOptim)
+  # U is a (Kx+Ky-2) x (Kx+Ky-1) block matrix of zeros as base
+  # from top left to bottom right, we have the following three submatrices:
+  # for \rho: [1:2, 1]
+  # for X-submatrix: [(2+1):(Kx-2+2), (1+1):(Kx-1+1)] = [3:Kx,2:Kx] is (Kx-2) x (Kx-1)
+  # for Y-submatrix: [(2+(Kx-2)+1):(Kx+Ky-2), (1+(Kx-1)+1):(Kx+Ky-1)] = [(Kx+1):Km2, (Kx+1):Km1] is (Ky-2) x (Ky-1)
+  U    <- matrix(0L, nrow = Km2, ncol = Km1)
+  subx <- constrOptim_submatrix(Kx)
+  suby <- constrOptim_submatrix(Ky)
+  U[1:2,1]                  <- c(1L, -1L) # enforce \rho \in [-1,1]
+  U[3:Kx, 2:Kx]             <- subx       # enforce monotonicity of X-thresholds
+  U[(Kx+1):Km2, (Kx+1):Km1] <- suby       # enforce monotonicity of Y-thresholds
+  
+  ### the C vector (ci in ?stats::constrOptim)
+  C        <- rep(NA_real_, Km2)
+  C[1:2]   <- -(1.0 - tol_rho) # tolerance for \rho \in [-1,1]
+  C[3:Km2] <- tol_thresholds # tolerance for monotonicity of thresholds
+  
+  return(list(ui = U, ci = C))
+  
+} # FUN
+
+
+# submatrix that imposes the strictly increasing monotonicity of the thresholds
+# downward stepwise pattern of c(-1,1)
+constrOptim_submatrix <- function(K)
+{
+  stopifnot(K > 2L) # fails if K = 2 (but no constraints needed there anyway)
+  
+  # initialize
+  Km1 <- K - 1L
+  Km2 <- K - 2L
+  A <- matrix(NA_integer_, nrow = Km2, ncol = Km1)
+  
+  for(i in seq_len(Km2))
+  {
+    tmp <- rep(0L, Km1)
+    tmp[i:(i+1L)] <- c(-1L, 1L)
+    A[i,] <- tmp
+  } # FOR
+  return(A)
+}
+
+
+# give names to the dimensions of theta vector
+theta_names <- function(Kx, Ky)
+{
+  c("rho", paste0("a", 1:(Kx-1)), paste0("b", 1:(Ky-1)))
+}
