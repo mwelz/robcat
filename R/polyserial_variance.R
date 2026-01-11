ac4694d005f50acb41b8869416f0697ac99cdf47
mwelz <max.welz@uzh.ch>
2026-01-11 23:03:45 +0100

    reorganized variance code and also took care of edge case in numerical solver

--- R/polyserial_variance.R
+++ R/polyserial_variance.R
@@ -1,262 +1,8 @@
-## functions for the ASV
-
-#### first order derivatives: all scalar-input, vectorize later in componentwise integral! ### ----
-
-
-## u_theta(z): derivative of log p_theta(z) wrt theta
-score_z <- function(x, y, rho, mu, sigma, thres, num_y)
-{
-  ## We run into a divide-by-zero issue for extremely large values of x because
-  ## densities are (computationally) zero at such values, regardless of parameter
-  ## values. To avoid this issue, return 0 if standardized x exceeds value 15,
-  ## which has virtually zero probability mass at the assumed normal model
-  if(abs((x - mu) / sigma) > 10) # TODO: maybe more general is better: as soon as p = 0
-  {
-    out <- rep(0.0, num_y + 2L)
-  } else
-  {
-    ## densities
-    p_cond <- py_x_scalars(y = y, x = x, rho = rho, mu = mu, sigma = sigma, thres = thres, num_y = num_y)
-    p_marg <- px(x = x, mu = mu, sigma = sigma)
-    
-    ## gradients
-    p_cond_grad <- py_x_grad(y = y, x = x, rho = rho, mu = mu, sigma = sigma, thres = thres, num_y = num_y)
-    p_marg_grad <- px_grad(x = x, mu = mu, sigma = sigma, num_y = num_y)
-    
-    out <- p_cond_grad / p_cond + p_marg_grad / p_marg
-  } # IF
-  
-  return(out)
-} # FUN
-
-
-## derivative of score wrt theta
-# approximate numerically for now
-# NB: behavior of score_z for large x could cause issues again
-Qz <- function(x, y, rho, mu, sigma, thres, num_y)
-{
-  theta <- c(rho, mu, sigma^2, thres)
-  fn <- function(theta)
-  {
-    score_z(x = x, y = y, 
-            rho = theta[1L],
-            mu = theta[2L], 
-            sigma = sqrt(theta[3L]), 
-            thres = theta[4L:(num_y+2L)],
-            num_y = num_y)
-  }
-  
-  jac <- numDeriv::jacobian(fn, theta)
-  return(jac * (-1.0))
-}
-
-
-# ## for the integral in J-hat
-# J_integrand <- function(x, rho, mu, sigma, thres, num_y, alphaplus1)
-# {
-#   p_marg_alphap1 <- px(x = x, mu = mu, sigma = sigma)^alphaplus1
-#   num_params <- num_y + 2L
-#   matsum <- matrix(0.0, nrow = num_params, ncol = num_params)
-#   
-#   for(y in seq_len(num_y))
-#   {
-#     p_cond_alphap1 <- py_x_scalars(y = y, x = x, rho = rho, mu = mu, 
-#                                    sigma = sigma, thres = thres, num_y = num_y)
-#     
-#     u <- score_z(x = x, y = y, rho = rho, mu = mu, sigma = sigma, thres = thres, num_y = num_y)
-#     Q <- Qz(x = x, y = y, rho = rho, mu = mu, sigma = sigma, thres = thres, num_y = num_y)
-#     
-#     summand <- p_cond_alphap1 * (alphaplus1 * u %*% t(u) - Q)
-#     matsum <- matsum + summand
-#   } # FOR
-#   
-#   return(p_marg_alphap1 * matsum)
-# }
-# 
-# 
-# ##  solve the integral in Jhat component-wisely
-# # TODO: this is very slow, can be improved a lot!
-# Jhat_integral <- function(rho, mu, sigma, thres, num_y, alphaplus1)
-# {
-#   ## initialize
-#   num_params <- num_y + 2L
-#   mat_out <- matrix(NA_real_, num_params, num_params)
-#   
-#   for(i in seq_len(num_params))
-#   {
-#     ## j: j >= i
-#     for(j in which(1:num_params >= i))
-#     {
-#       ## function that picks out (i,j)-th coordinate of the integrand matrix
-#       fn <- function(x) {
-#         J_integrand(x = x, rho = rho, mu = mu, sigma = sigma, thres = thres, num_y = num_y, alphaplus1 = alphaplus1)[i,j]
-#       }
-#       
-#       ## solve integral at coordinate (i,j)
-#       # FIXME: handling of large x values is still a bit shakey, see score fun!
-#       # Also, this is shittily programmed: very wasteful since in each iteration we compute the entire matrix just to drop everything except component (i,j). can be sped up a lot!
-#       integrated <- stats::integrate(Vectorize(fn), lower = -Inf, upper = Inf)$value
-#       mat_out[i,j] <- mat_out[j,i] <- integrated
-#       
-#     } # FOR i
-#   } # FOR j
-#   
-#   return(mat_out)
-# } # FUN
-
-
-# order: rho, mu, sigma2, thres
-logl_scalar <- function(theta, x, y, num_params, num_y)
-{
-  rho <- theta[1L]
-  mu <- theta[2L]
-  sigma2 <- theta[3L]
-  thres <- theta[4L:num_params]
-  sigma <- sqrt(sigma2)
-  
-  marg <- px(x = x, mu = mu, sigma = sigma)
-  cond <- py_x_scalars(y = y, x = x, rho = rho, mu = mu, sigma = sigma, thres = thres, num_y = num_y)
-  prob <- marg * cond
-  out <- ifelse(prob > 0.0, log(prob), 0.0) # pathological cases with 0 prob get 0 likelihood (e.g. Inf; cancels anyway)
-  return(out)
-}
-
-
-## first derivative of logl wrt i-th parameter
-score_i <- function(theta, i, x, y, num_params, num_y)
-{
-  theta_before_i <- theta[seq_len(i-1L)]
-  theta_after_i <- theta[i + seq_len(num_params - i)]
-  
-  fn <- function(thetai) logl_scalar(theta = c(theta_before_i, thetai, theta_after_i),
-                                     x = x, y = y, num_params = num_params, num_y = num_y)
-  
-  return(numDeriv::grad(fn, x = theta[i]))
-}
-
-## multiply i-th score by j-th score
-crossscore <- function(theta, i, j, x, y, num_params, num_y)
-{
-  ui <- score_i(theta = theta, i = i, x = x, y = y, num_params = num_params, num_y = num_y)
-  
-  if(i == j)
-  {
-    uj <- ui
-  } else
-  {
-    uj <- score_i(theta = theta, i = j, x = x, y = y, num_params = num_params, num_y = num_y)
-  }
-  
-  return(ui * uj)
-}
-
-
-## negative second derivative of logl wrt i-th parameter
-Qii <- function(theta, i, x, y, num_params, num_y)
-{
-  theta_before_i <- theta[seq_len(i-1L)]
-  theta_after_i <- theta[i + seq_len(num_params - i)]
-  
-  fn <- function(thetai) logl_scalar(theta = c(theta_before_i, thetai, theta_after_i),
-                                     x = x, y = y, num_params = num_params, num_y = num_y)
-  
-  return(-as.numeric(numDeriv::hessian(fn, x = theta[i])))
-}
-
-
-## negative cross-derivatives of logl
-# i <= j!!!
-Qij <- function(theta, i, j, x, y, num_params, num_y)
-{
-  if(i == j)
-  {
-    out <- Qii(theta = theta, i = i, x = x, y = y, num_params = num_params, num_y = num_y)
-  } else if (i < j)
-  {
-    theta_before_i <- theta[seq_len(i-1L)]
-    theta_btw_i_and_j <- theta[i + seq_len(j - i - 1L)]
-    theta_after_j <- theta[j + seq_len(num_params - j)]
-    
-    fn <- function(thetaij) logl_scalar(theta = c(theta_before_i, thetaij[1], theta_btw_i_and_j, thetaij[2], theta_after_j),
-                                        x = x, y = y, num_params = num_params, num_y = num_y)
-    out <- -numDeriv::hessian(fn, x = c(theta[i], theta[j]))[1,2]
-  } else stop(" i <= j violated")
-  
-  return(out)
-}
-
-
-
-#score_z(x = x, y = y, rho = rho, mu = mu, sigma = sigma, thres = thres, num_y = num_y)
-#sapply(1:num_params, function(i) score_i(theta = theta, i = i, x = x, y = y, num_y = num_y, num_params = num_params)) # yay
-
-
-
-## for the integral in J-hat: x is a scalar
-J_integrand_ij <- function(theta, i, j, x, num_params, num_y, alphaplus1)
-{
-  rho <- theta[1L]
-  mu <- theta[2L]
-  sigma2 <- theta[3L]
-  thres <- theta[4L:num_params]
-  sigma <- sqrt(sigma2)
-  p_marg_alphap1 <- px(x = x, mu = mu, sigma = sigma)^alphaplus1
-  
-  ## init
-  out <- 0.0
-  
-  for(y in seq_len(num_y))
-  {
-    p_cond_alphap1 <- py_x_scalars(y = y, x = x, rho = rho, mu = mu, 
-                                   sigma = sigma, thres = thres, num_y = num_y)^alphaplus1
-    
-    uiuj. <- crossscore(theta = theta, i = i, j = j, x = x, y = y, num_params = num_params, num_y = num_y)
-    Qij. <- Qij(theta = theta, i = i, j = j, x = x, y = y, num_params = num_params, num_y = num_y)
-    
-    summand <- p_cond_alphap1 * (alphaplus1 * uiuj. - Qij.)
-    out <- out + summand
-  } # FOR
-  
-  return(p_marg_alphap1 * out)
-}
-
-
-## the 1st part in Jhat (the integral one): matrix A
-Jhat_integral <- function(rho, mu, sigma, thres, num_y, alphaplus1)
-{
-  ## initialize
-  num_params <- num_y + 2L
-  mat_out <- matrix(NA_real_, num_params, num_params)
-  theta <- c(rho, mu, sigma^2, thres)
-  
-  
-  for(i in seq_len(num_params))
-  {
-    ## j: j >= i
-    for(j in which(1:num_params >= i))
-    {
-      
-      ## function to solve integral over for dimension (i,j)
-      fn <- Vectorize(function(x) {
-        J_integrand_ij(theta = theta, i = i, j = j, x = x, num_params = num_params, num_y = num_y, alphaplus1 = alphaplus1)
-      })
-      
-      integrated <- stats::integrate(fn, lower = -Inf, upper = Inf)$value
-      mat_out[i,j] <- mat_out[j,i] <- integrated
-      
-    } # FOR i
-  } # FOR j
-  
-  return(mat_out)
-} # FUN
-
-
-## the 2nd part in Jhat (the empirical one): matrix Bhat
 # exception: here x and y are vectors of observations, no longer scalars
-Jhat_empirical <- function(x, y, rho, mu, sigma, thres, num_y, alpha)
+get_matrix_B <- function(x, y, rho, mu, sigma2, thres, num_y, alpha)
 {
-  
   ## calculate densities at empirical observations
+  sigma <- sqrt(sigma2)
   p_marg_alpha <- px(x = x, mu = mu, sigma = sigma)^alpha
   p_cond_alpha <- py_x_vectorized(
     y = y, x = x, rho = rho, mu = mu, sigma = sigma, thres = thres, num_y = num_y)^alpha
@@ -268,32 +14,148 @@ Jhat_empirical <- function(x, y, rho, mu, sigma, thres, num_y, alpha)
   
   for(i in seq_len(N))
   {
-    Q <- Qz(x = x[i], y = y[i], rho = rho, mu = mu, sigma = sigma, thres = thres, num_y = num_y)
-    u <- score_z(x = x[i], y = y[i], rho = rho, mu = mu, sigma = sigma, thres = thres, num_y = num_y)
+    Q <- polyserial_Qmat(x = x[i], y = y[i], rho = rho, mu = mu, 
+                         sigma2 = sigma2, thres = thres, num_y = num_y)
+    u <- score_z(x = x[i], y = y[i], rho = rho, mu = mu, sigma = sigma, 
+                 thres = thres, num_y = num_y)
     
     increment <- p_marg_alpha[i] * p_cond_alpha[i] * (Q - alpha * u %*% t(u))
     out_mat <- out_mat + increment
   }
   return(out_mat / N)
-} # FUN
+}
+
+
+get_matrix_A <- function(rho, mu, sigma2, thres, num_y, alphaplus1)
+{
+  ## initialize
+  num_params <- num_y + 2L
+  mat_out <- matrix(NA_real_, num_params, num_params)
+
+
+  for(i in seq_len(num_params))
+  {
+    ## get functions
+    px_d1_i   <- get_px_d1_i(i)
+    py_x_d1_i <- get_py_x_d1_i(i)
+    
+    ## j: j >= i
+    for(j in which(1:num_params >= i))
+    {
+      ## get functions
+      px_d1_j    <- get_px_d1_i(j)
+      py_x_d1_j  <- get_py_x_d1_i(j)
+      px_d2_ij   <- get_px_d2_ij(i,j)
+      py_x_d2_ij <- get_py_x_d2_ij(i,j)
+      
+      ## function to solve integral over for dimension (i,j)
+      fn <- Vectorize(function(x) {
+        A_ij_integrand_x(x = x, rho = rho, mu = mu, 
+                         sigma2 = sigma2, thres = thres, num_y = num_y, 
+                         alphaplus1 = alphaplus1, 
+                         px_d1_i = px_d1_i, px_d1_j = px_d1_j, 
+                         py_x_d1_i = py_x_d1_i, py_x_d1_j = py_x_d1_j, 
+                         px_d2_ij = px_d2_ij, py_x_d2_ij = py_x_d2_ij)
+      }) 
+      
+      ## solve integral
+      # in rare cases, stats::integrate throws a cryptic error. Using 
+      # a different solver seems to fix the issue
+      # FIXME: it seems that fn() sometimes returns an NaN, which triggers
+      # the issue. I suspect this is caused by a 0 * Inf operation somewhere in 
+      # the finite grid we evaluate over; investigate!
+      integrated <- try(
+        stats::integrate(fn, -Inf, Inf)$value, 
+        silent = TRUE
+      )
+      if (inherits(integrated, "try-error"))
+      {
+        invisible(utils::capture.output(
+          integrated <- pracma::integral(fn, -Inf, Inf)))
+        
+      }
+
+      mat_out[i,j] <- mat_out[j,i] <- integrated
+    } # FOR i
+  } # FOR j
+  
+  return(mat_out)
+}
+
+
+A_ij_integrand_x <- function(x, rho, mu, sigma2, thres, num_y, 
+                             alphaplus1,
+                             px_d1_i, 
+                             px_d1_j,
+                             py_x_d1_i,
+                             py_x_d1_j,
+                             px_d2_ij,
+                             py_x_d2_ij)
+{
+  ## marginal information
+  sigma <- sqrt(sigma2)
+  marg_p <- px(x = x, mu = mu, sigma = sigma)
+  marg_p_ap1 <- marg_p^alphaplus1
+  marg_p_inv <- ifelse(marg_p > 0.0, 1.0 / marg_p, 0.0) # avoid divide by 0
+  marg_grad_i <- px_d1_i(x = x, mu = mu, sigma2 = sigma2)
+  marg_grad_j <- px_d1_j(x = x, mu = mu, sigma2 = sigma2)
+  marg_hess_ij <- px_d2_ij(x = x, mu = mu, sigma2 = sigma2)
+  marg_score_i <- marg_p_inv * marg_grad_i
+  marg_score_j <- marg_p_inv * marg_grad_j
+  marg_score_prod <- marg_score_i * marg_score_j
+  marg_Qij <- marg_score_prod - marg_p_inv * marg_hess_ij
+  
+  ## init
+  out <- 0.0
+  
+  for(y in seq_len(num_y))
+  {
+    cond_p <- py_x_scalars(y = y, x = x, rho = rho, mu = mu, 
+                           sigma = sigma, thres = thres, num_y = num_y)
+    cond_p_ap1 <- cond_p^alphaplus1
+    cond_p_inv <- ifelse(cond_p > 0.0, 1.0 / cond_p, 0.0) # avoid divide by 0
+    cond_grad_i <- py_x_d1_i(y = y, x = x, rho = rho, mu = mu, 
+                             sigma2 = sigma2, thres = thres, num_y = num_y)
+    cond_grad_j <- py_x_d1_j(y = y, x = x, rho = rho, mu = mu, 
+                             sigma2 = sigma2, thres = thres, num_y = num_y)
+    cond_hess_ij <- py_x_d2_ij(y = y, x = x, rho = rho, mu = mu, 
+                               sigma2 = sigma2, thres = thres, num_y = num_y)
+    cond_score_i <- cond_p_inv * cond_grad_i
+    cond_score_j <- cond_p_inv * cond_grad_j
+    cond_score_prod <- cond_score_i * cond_score_j
+    cond_Qij <- cond_score_prod - cond_p_inv * cond_hess_ij
+    
+    ## assemble final summand
+    Q_ij <- cond_Qij + marg_Qij
+    uiuj <- (marg_score_i + cond_score_i) * (marg_score_j + cond_score_j)
+    increment <- cond_p_ap1 * (alphaplus1 * uiuj - Q_ij)
+    
+    ## update
+    out <- out + increment
+  }
+  return(marg_p_ap1 * out)
+}
 
 
 # exception: here x and y are vectors of observations, no longer scalars
-get_Jhat <- function(x, y, rho, mu, sigma, thres, num_y, alpha)
+get_Jhat <- function(x, y, rho, mu, sigma2, thres, num_y, alpha)
 {
-  A <- Jhat_integral(
-    rho = rho, mu = mu, sigma = sigma, thres = thres, num_y = num_y, alphaplus1 = alpha + 1.0)
-  B <- Jhat_empirical(
-    x = x, y = y, rho = rho, mu = mu, sigma = sigma, thres = thres, num_y = num_y, alpha = alpha)
+  A <- get_matrix_A(
+    rho = rho, mu = mu, sigma2 = sigma2, thres = thres, 
+    num_y = num_y, alphaplus1 = alpha + 1.0)
+  B <- get_matrix_B(
+    x = x, y = y, rho = rho, mu = mu, sigma2 = sigma2, thres = thres,
+    num_y = num_y, alpha = alpha)
   return(A + B)
 }
 
 
 # exception: here x and y are vectors of observations, no longer scalars
-get_K_xihat <- function(x, y, rho, mu, sigma, thres, num_y, alpha)
+get_K_xihat <- function(x, y, rho, mu, sigma2, thres, num_y, alpha)
 {
   
   ## densities
+  sigma  <- sqrt(sigma2)
   p_marg <- px(x = x, mu = mu, sigma = sigma)
   p_cond <- py_x_vectorized(y = y, x = x, rho = rho, mu = mu, sigma = sigma,
                             thres = thres, num_y = num_y)
@@ -311,7 +173,7 @@ get_K_xihat <- function(x, y, rho, mu, sigma, thres, num_y, alpha)
   ## xi hat
   summands_xi <- sapply(seq_len(N), function(i){
     prob_alpha[i] * score_vecs[,i]
-    })
+  })
   xihat <- rowMeans(summands_xi)
   
   ## K hat: initialize
@@ -324,19 +186,19 @@ get_K_xihat <- function(x, y, rho, mu, sigma, thres, num_y, alpha)
     summand <- prob_alpha2[i] * u %*% t(u)
     summands_K <- summands_K + summand
   } # FOR
-    
+  
   Khat <- summands_K / N - xihat %*% t(xihat)
- 
+  
   return(list(Khat = Khat, xihat = xihat))
 } # FUN
 
 
 # exception: here x and y are vectors of observations, no longer scalars
-get_Sigmahat_polyserial <- function(x, y, rho, mu, sigma, thres, num_y, alpha)
+get_Sigmahat_polyserial <- function(x, y, rho, mu, sigma2, thres, num_y, alpha)
 {
-  J <- get_Jhat(x = x, y = y, rho = rho, mu = mu, sigma = sigma, 
+  J <- get_Jhat(x = x, y = y, rho = rho, mu = mu, sigma2 = sigma2, 
                 thres = thres, num_y = num_y, alpha = alpha)
-  K <- get_K_xihat(x = x, y = y, rho = rho, mu = mu, sigma = sigma,
+  K <- get_K_xihat(x = x, y = y, rho = rho, mu = mu, sigma2 = sigma2,
                    thres = thres, num_y = num_y, alpha = alpha)$Khat
   
   Jinv <- solve(J)
@@ -357,8 +219,9 @@ variance_polyserial <- function(theta, x, y, alpha)
   tau <- theta[4L:d]
   
   # ASV of sqrt(N) * thetahat
-  asv <- get_Sigmahat_polyserial(x = x, y = y, rho = rho, mu = mu, sigma = sqrt(sigma2),
-                                 thres = tau, num_y = num_y, alpha = alpha)
+  asv <- get_Sigmahat_polyserial(
+    x = x, y = y, rho = rho, mu = mu, sigma2 = sigma2,
+    thres = tau, num_y = num_y, alpha = alpha)
   
   # ASV of thetahat
   vcov <- asv / N
@@ -368,6 +231,7 @@ variance_polyserial <- function(theta, x, y, alpha)
   
   return(list(se = se, vcov = vcov))
 }
+
 
 
 variance_polyserial_mle <- function(theta, x, y)
@@ -401,36 +265,19 @@ variance_polyserial_mle <- function(theta, x, y)
   return(list(se = se, vcov = vcov))
 }
 
-# keep y fixed and have x vectorized
-#py_x_vectorized_x <- Vectorize(py_x_scalars, vectorize.args = "x")
 
-# x = 1.548
-# y <- 3L
-# rho <- 0.5
-# sigma <- 1
-# sigma2 <- sigma^2
 # mu <- 0
-# thres <- c(-1.5, -0.5, 0.5, 1.5)
+# sigma2 <- 1.2
+# rho <- 0.5
+# thres <- c(-1, 0, 1,1.5)
+# y_set <- c(1L, 2L, 3L, 4L, 5L)
+# num_y <- length(y_set)
 # theta <- c(rho, mu, sigma2, thres)
-# num_y <- 5L
-# num_params <- num_y + 2L
-# i <- 5L
-# j <- 7L
-# 
-# Qz(x=x, y=y, rho = rho, mu = mu, sigma = sigma, thres = thres, num_y = num_y)[i,j]
-# Qij(theta = theta, i = i, j = j, x = x, y = y, num_params = num_params, num_y = num_y)
-
-
-
-
-## TODO: double check Qz, always returns 0 in certain cells (probably taus that are uncorrelated)
-
-## TODO: test score function numerically for correctness!
-# once you have that, test the elementwise integral strategy by solving the expectation integral over the score at true theta, should be zero!
-
-## tester for correctness!
-# TODO: funs depend on sigma, bbut the param is theta^2! needs to be handled as below:
-# THIS IS IMPORTANT FOR NUMERIC APPROX OF FISHER INFO!
-# f = function(theta) py_x_scalars(y,x,theta[1],theta[2],sqrt(theta[3]),theta[4:7], num_y)
-# numDeriv::grad(f, theta_true)
-# py_x_grad(y,x,rho,mu,sigma,thres,num_y)
+# alphaplus1 = 1.1
+# i = 1L
+# j = 2L
+# d <- length(theta)
+# set.seed(1)
+# x <- rnorm(n = 1, mean = mu, sd = sqrt(sigma2))
+# f = get_px_d1_i(4L)
+# f(x,mu,sigma2)
